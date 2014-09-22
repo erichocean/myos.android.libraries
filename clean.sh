a318a1d0b8a838aa9ceedab3e8e008dc157ee9d0
Amr Aboelela <amraboelela@gmail.com>
2014-09-22 15:38:27 -0700

    Commented libraries in cleab.sh

--- clean.sh
+++ clean.sh
@@ -2,65 +2,65 @@
 # Copyright 2014 myOS Group. All rights reserved.
 #
 
-echo
-echo "****************************** Cleaning libraries ******************************"
+#echo
+#echo "****************************** Cleaning libraries ******************************"
 
-cd objc
-make clean
-cd ..
-
-cd icu
-make clean
-cd ..
-
-cd pixman
-make clean
-cd ..
-
-cd png
-make clean
-cd ..
-
-cd freetype
-make clean
-cd ..
-
-cd expat
-make clean
-cd ..
-
-cd fontconfig
-make clean
-cd ..
-
-cd cairo
-make clean
-cd ..
-
-cd lcms
-make clean
-cd ..
-
-#cd MAGLESv1_enc
+#cd objc
 #make clean
 #cd ..
 
-#cd MARenderControl_enc
+#cd icu
 #make clean
 #cd ..
 
-#cd MAOpenglSystemCommon
+#cd pixman
 #make clean
 #cd ..
 
-#cd MAOpenglCodecCommon
+#cd png
 #make clean
 #cd ..
 
-cd MAEGL
-make clean
-cd ..
+#cd freetype
+#make clean
+#cd ..
 
-cd MAKit
-make clean
-cd ..
+#cd expat
+#make clean
+#cd ..
+
+#cd fontconfig
+#make clean
+#cd ..
+
+#cd cairo
+#make clean
+#cd ..
+
+#cd lcms
+#make clean
+#cd ..
+
+##cd MAGLESv1_enc
+##make clean
+##cd ..
+
+##cd MARenderControl_enc
+##make clean
+##cd ..
+
+##cd MAOpenglSystemCommon
+##make clean
+##cd ..
+
+##cd MAOpenglCodecCommon
+##make clean
+##cd ..
+
+#cd MAEGL
+#make clean
+#cd ..
+
+#cd MAKit
+#make clean
+#cd ..
